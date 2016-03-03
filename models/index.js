7f0bd528cfe8b14ee89523a20c98c3d3b61c97e6
takshingli810 <takshingli810@gmail.com>
2016-03-03 15:40:12 -0800

    fixed index.js in models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely");
+process.on('exit', function() { mongoose.disconnect(); });
+
+module.exports.Album = require("./album");
+module.exports.Track = require("./track");
